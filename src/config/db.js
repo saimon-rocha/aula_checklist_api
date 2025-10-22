b371ba34a1bebb3e065cf8611811ed4e0c862ea6
saimon-rocha <saimon.rocha07@gmail.com>
2025-10-22 18:51:03 -0300

    ajustado conexao com render

--- src/config/db.js
+++ src/config/db.js
@@ -1,35 +1,24 @@
 import 'dotenv/config'; // carrega o .env automaticamente
-const useSSL =
-  (process.env.DATABASE_HOST || process.env.DB_HOST || "").includes("render");
 
-// export default {
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-//   dialect: process.env.DB_DIALECT ? process.env.DB_DIALECT.replace(/'/g, '') : 'postgres',
-//   dialectOptions: process.env.DB_SSL === 'true' ? { ssl: { require: true, rejectUnauthorized: false } } : {},
-// };
+// Detecta se precisa de SSL
+const useSSL =
+  (process.env.DATABASE_HOST || "").includes("render") || process.env.DATABASE_SSL === 'true';
 
 // Produção
 export default {
-  host: process.env.DATABASE_HOST || process.env.DB_HOST || "localhost",
-  database: process.env.DATABASE_NAME || process.env.DB_NAME || "",
-  username: String(process.env.DATABASE_USER || process.env.DB_USER || ""),
-  password: String(process.env.DATABASE_PASSWORD || process.env.DB_PASS || ""),
-  dialect: process.env.DATABASE_DIALECT || process.env.DB_DIALECT || "postgres",
-  port: Number(process.env.DATABASE_PORT || process.env.DB_PORT || 5432),
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  username: String(process.env.DATABASE_USER),
+  password: String(process.env.DATABASE_PASSWORD),
+  dialect: process.env.DATABASE_DIALECT || 'postgres',
+  port: Number(process.env.DATABASE_PORT || 5432),
   dialectOptions: useSSL
     ? {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    }
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
     : undefined,
   logging: false,
 };
-
-
-
